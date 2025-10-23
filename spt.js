cdc6fdcdd53ccc28c743c53aad14d91293dcc2a6
Siva-2517 <sivasurya6430@gmail.com>
2025-10-23 14:41:47 +0530

    fir

---
+++ spt.js
@@ -0,0 +1,34 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  navbar.classList.toggle('active');   // toggle navbar visibility
+  menuIcon.classList.toggle('bx-x');   // toggle menu icon (burger ↔ X)
+};
+
+document.querySelectorAll('.navbar a').forEach(link => {
+  link.onclick = () => {
+    navbar.classList.remove('active');
+    menuIcon.classList.remove('bx-x');
+  };
+});
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(link => {
+        link.classList.remove("active");
+        document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
+      });
+    }
+  });
+};
+
